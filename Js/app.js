4f8bc943e1324eb0ab1de8cf2fa9ee68c7fe84dc
asyamoh <asia_nazif@yahoo.com>
2021-05-24 14:20:30 +0300

    newproject

---
+++ Js/app.js
@@ -0,0 +1,30 @@
+
+'use strict'
+
+let pet = prompt ('do you think I am a cat person?') ;
+
+pet = pet.toUpperCase('')
+
+if (pet == 'YES' || pet == 'Y') {
+       alert('YOU ARE WRONG!');
+    console.log('WRONG'+pet)
+    
+     } else if (pet == 'NO' || pet == 'N') {
+     
+         alert ('Hurrayy!right')
+         console.log('RIGHT!'+firstQue)
+    }
+
+         var month = prompt('is my favourite moth Nov?');
+ month = month.toUpperCase()
+ if (month == 'YES' || month == 'Y') {
+   alert('sry, wrong answer');
+
+} else if (month == 'NO' || month == 'N') {
+   //console.log('great'+month)
+     alert('wow!how do you know are we a close friends or what?')
+
+ }
+
+
+        
