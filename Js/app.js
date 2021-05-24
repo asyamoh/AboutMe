0e392d1fa37d1b78a7cd86da5d767c3b0843f23a
asyamoh <asia_nazif@yahoo.com>
2021-05-24 14:34:06 +0300

    newupdate

--- Js/app.js
+++ Js/app.js
@@ -15,16 +15,16 @@ if (pet == 'YES' || pet == 'Y') {
          console.log('RIGHT!'+firstQue)
     }
 
-         var month = prompt('is my favourite moth Nov?');
- month = month.toUpperCase()
- if (month == 'YES' || month == 'Y') {
-   alert('sry, wrong answer');
+       let month = prompt ('is November my fvrt month of the year ?');
+       month = month.toUpperCase('')
+       if (month == 'YES'|| month == 'Y'){
+           alert('wow! are we a close friends or something how did you know :P');
+           console.log('RIGHT!'+ month)
+       } else if (month == 'NO'|| 'N') {
 
-} else if (month == 'NO' || month == 'N') {
-   //console.log('great'+month)
-     alert('wow!how do you know are we a close friends or what?')
-
- }
+        alert('it is very clear that we do not know each other :(')
+        console.log('WRONGG' + month)
+       }
 
 
         
